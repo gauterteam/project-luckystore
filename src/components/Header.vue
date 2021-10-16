e032b42d71aab372dceb3518cbbc3f85e0ecd9c8
ShaXBoZ AliQuLoV <gauterteam@gmail.com>
2021-10-16 19:35:28 +0500

    shaxboz-19-33-16-10-2021

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="header">Header</div>
+  <div class="header">
+    <div class="multicolor">
+      <div class="colorline line-1"></div>
+      <div class="colorline line-2"></div>
+      <div class="colorline line-3"></div>
+      <div class="colorline line-4"></div>
+      <div class="colorline line-5"></div>
+      <div class="colorline line-6"></div>
+      <div class="colorline line-7"></div>
+      <div class="colorline line-8"></div>
+      <div class="colorline line-9"></div>
+      <div class="colorline line-10"></div>
+      <div class="colorline line-11"></div>
+      <div class="colorline line-12"></div>
+      <div class="colorline line-13"></div>
+      <div class="colorline line-14"></div>
+      <div class="colorline line-15"></div>
+      <div class="colorline line-16"></div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,5 +30,10 @@ export default {
 <style scoped lang="scss">
 .header {
   height: 270px;
+}
+
+.colorline {
+  height: 4px;
+  width: 7%;
 }
 </style>
